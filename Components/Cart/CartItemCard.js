c967a444d7089b379038cf19f69c05e0f7c3de97
Imran <imranmohammad098@gmail.com>
2023-05-21 20:25:35 +0530

    Added sizes in the product card

--- Components/Cart/CartItemCard.js
+++ Components/Cart/CartItemCard.js
@@ -1,11 +1,14 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import CartContext from "../../Utils/cartContext";
 
 const CartItemCard = ({cartItemDetails,key})=>{
     const {cartItem,setCartItem} = useContext(CartContext);
-    console.log(cartItemDetails);
     console.log(cartItem);
-    localStorage.setItem("hoodiehubcart",JSON.stringify(cartItem));
+    useEffect(()=>{
+        console.log("cartitems ",cartItem);
+        localStorage.setItem("hoodiehubcart",JSON.stringify(cartItem));
+    },[cartItem])
+    
     return(
         <div className="flex flex-row m-3">
             <img src={cartItemDetails.img[0].imgurl} className="w-20"/>
@@ -13,8 +16,8 @@ const CartItemCard = ({cartItemDetails,key})=>{
                 <h1>{cartItemDetails.brand}</h1>
                 <h1>{cartItemDetails.proddescription}</h1>
                 <div className="flex flex-row">
-                    <h1 className="text-md">RS.{cartItemDetails.discprice}</h1>
-                    <h1 className="text-sm line-through ml-1">RS.{cartItemDetails.mrp}</h1>
+                    <h1 className="text-md">₹{cartItemDetails.discprice}</h1>
+                    <h1 className="text-sm line-through ml-1">₹{cartItemDetails.mrp}</h1>
                     <h1>{`(${cartItemDetails.discpercentage})%`}</h1>
                 </div>
                 <button className="bg-black text-sm text-white p-1 rounded-md"
@@ -22,7 +25,6 @@ const CartItemCard = ({cartItemDetails,key})=>{
                     setCartItem({
                         cartitems : cartItem.cartitems.filter((x)=>x.id!=cartItemDetails.id)
                     })
-                    
                 }}
                 >Delete Item</button>
             </div>
--- Components/Header/NavBar.js
+++ Components/Header/NavBar.js
@@ -7,7 +7,7 @@ const NavBar = ()=>{
     return (
         <div className="flex flex-row rounded-md">
             <Link to="/" className="m-2 mt-auto p-2 hover:bg-black hover:rounded-md hover:text-white transition-shadow">Home</Link>
-            <Link to="/" className="m-2 mt-auto p-2 hover:bg-black hover:rounded-md hover:text-white transition-shadow">About</Link>
+            <a href="#about" className="m-2 mt-auto p-2 hover:bg-black hover:rounded-md hover:text-white transition-shadow">About</a>
             <Link to="/cart" className="m-2 mt-auto p-2 hover:bg-black hover:rounded-md hover:text-white transition-shadow">Cart {cartItem?.cartitems?.length===0?"":cartItem?.cartitems?.length}</Link>
         </div>
     )
