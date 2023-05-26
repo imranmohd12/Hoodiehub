6246e24dc9643369ca5b045cd7cb84ac68143574
Imran <imranmohammad098@gmail.com>
2023-05-26 22:17:49 +0530

    sample push

--- Utils/constants.js
+++ Utils/constants.js
@@ -1,4 +1,3 @@
-
 export const IMG_URL = "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/21203458/2023/1/2/06271946-be59-476d-bb62-0b4d81c961611672651142670-Roadster-Men-Sweatshirts-91672651142282-1.jpg"
 export const SampleData = [
     {
